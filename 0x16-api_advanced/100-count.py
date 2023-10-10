39d5a544a4665d59504932f2dd520d33b964d248
Alex Jarabi <jarabialex@gmail.com>
2023-10-11 02:14:58 +0300

    Correction

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,7 +4,7 @@ import requests
 after = None
 
 
-def count_words(subreddit, word_list):
+def count_words(subreddit, word_list = []):
     """ Recursively queries the Reddit API, parses the title of all
         hot articles, and prints a sorted count of given keywords
         (case-insensitive, delimited by spaces) """
