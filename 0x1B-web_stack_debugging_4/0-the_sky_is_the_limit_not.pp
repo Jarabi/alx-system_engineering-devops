cbf20e51c10d101ece874d84e8b1fbaffaba950e
Alex Jarabi <jarabialex@gmail.com>
2023-10-22 02:47:32 +0300

    Final correction

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,25 +1,21 @@
 # Manifest to increate limit of files open for Nginx service
 
+# Make sure nginx is installed
+package { 'nginx':
+  ensure => installed,
+} ->
+
 # Raise limit for number of files nginx can open
-exec {'raise-nginx-limit':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx/',
-  path    => '/usr/local/bin:/bin/'
-}
-
-# Set number of nginx file descriptors
-exec { 'set-file-descriptors':
-  command => "sed -i '/events {/i worker_rlimit_nofile 100000;\n' /etc/nginx/nginx.conf",
-  path    => '/bin:/usr/bin'
-}
-
-# Set worker connections
-exec { 'set-worker-connections':
-  command => "sed -i 's/worker-connections [0-9]\\+/worker_connections 4000/' /etc/nginx/nginx.conf",
-  path    => '/bin:/usr/bin'
-}
+exec {'raise-ulimit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin:/bin/',
+  require => Package['nginx'],
+} ->
 
 # Restart nginx
 exec {'restart-nginx':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+  command     => 'nginx restart',
+  path        => '/etc/init.d/',
+  refreshonly => true,
+  subscribe   => Exec['raise-ulimit'],
 }
