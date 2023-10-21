b3f295bec06e6f3d33b7a32d1ef601e695aeb13c
Alex Jarabi <jarabialex@gmail.com>
2023-10-21 23:36:40 +0300

    Correction on manifest

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,9 @@
 # Manifest to increate limit of files open for Nginx service
 
 exec {'raise_limit':
-  command => "sed -i 's/-n 15/-n 4096/' /etc/default/nginx/",
-  path    => '/bin:/usr/bin'
+  command => "/bin/sed -i 's/15/4096/' /etc/default/nginx/"
 }
 
 exec {'restart_nginx':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+  command => '/usr/sbin/service nginx restart'
 }
