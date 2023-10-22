cf83fe8c71dc5e5d08883dc0f093ac6e3fb4c7ab
Alex Jarabi <jarabialex@gmail.com>
2023-10-22 03:07:11 +0300

    Final correction

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -6,16 +6,17 @@ package { 'nginx':
 } ->
 
 # Raise limit for number of files nginx can open
-exec {'raise-ulimit':
+exec { 'raise-ulimit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin:/bin/',
   require => Package['nginx'],
 } ->
 
 # Restart nginx
-exec {'restart-nginx':
+exec { 'restart-nginx':
   command     => 'nginx restart',
   path        => '/etc/init.d/',
   refreshonly => true,
   subscribe   => Exec['raise-ulimit'],
 }
+
